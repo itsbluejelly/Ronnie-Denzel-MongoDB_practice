18fb929987bf87207d220104061788eceb8725b5
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-12 13:29:59 +0300

    lesson_01_complete

---
+++ lesson_01_adding_files/server/controllers/rootController.js
@@ -0,0 +1,24 @@
+const eventLogger = require('../middleware/eventLogger')
+const BookModel = require('../model/Book')
+
+async function postController(req, res, next){
+    eventLogger(req.path, req.method, 'eventLogs.txt')
+
+    try{
+        const book = await BookModel.create(req.body)
+        res.status(201).send("Post was successful")
+        eventLogger("post to books collection was successful", book, 'databaseLogs.txt')
+    }catch(error){
+        res.status(400).json({
+            error: {
+                [error.name]: error.message
+            }
+        })
+
+        eventLogger(error.name, error.message, 'errorLogs.txt')
+    }
+
+    next()
+}
+
+module.exports = postController
---
+++ lesson_01_adding_files/server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const eventLogger = require('./middleware/eventLogger')
+const connectDB = require('./config/connectDB')
+const rootRouter = require('./routers/rootRouter')
+
+const app = express()
+dotenv.config()
+connectDB()
+
+const port = process.env.PORT_NUMBER || 5500
+
+app.use(express.json())
+
+app.use("/", rootRouter)
+
+mongoose.connection.once('open', () => {
+    eventLogger("Connected to MongoDB", `Server listening on port ${port}`, 'databaseLogs.txt')
+    app.listen(port)
+})
