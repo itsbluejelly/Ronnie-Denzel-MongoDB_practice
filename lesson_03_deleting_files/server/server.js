e398eb86011982b129da1a0c14f5758ad50979d0
itsbluejelly <ronniedenzel0@gmail.com>
2023-07-13 13:29:40 +0300

    added_lesson_03 file tree

---
+++ lesson_03_deleting_files/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const eventLogger = require('./middleware/eventLogger')
+const connectDB = require('./config/connectDB')
+const booksPageRouter = require('./routers/booksPageRouter')
+const authorsPageRouter = require('./routers/authorsPageRouter')
+
+const app = express()
+dotenv.config()
+connectDB()
+
+const port = process.env.PORT_NUMBER || 5500
+
+app.use(express.json())
+
+app.use("/", booksPageRouter, authorsPageRouter)
+
+mongoose.connection.once('open', () => {
+    eventLogger("Connected to MongoDB", `Server listening on port ${port}`, 'databaseLogs.txt')
+    app.listen(port)
+})
